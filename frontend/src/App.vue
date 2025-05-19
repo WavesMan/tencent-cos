1b27e2efd04752759b2b114b0df371048dbcd978
WavesMan <1690611689@qq.com>
2025-05-19 19:53:24 +0800

    refactor(styles): 将内联样式移至外部CSS文件
    将App.vue中的内联CSS样式提取到单独的App-light.css文件，提高代码可维护性

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,17 +5,5 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
+@import './assets/css/App-light.css';
 </style>
